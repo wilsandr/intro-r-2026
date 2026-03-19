b982da80508166b71b0f7017f53ef8b25b3824e1
Wilson <WilsAndr@trimet.org>
2026-03-19 11:15:54 -0700

    import data added summary functions

--- R/Intro Data Summary.R
+++ R/Intro Data Summary.R
@@ -17,3 +17,21 @@ tail(raw_15min)
 summary(raw_15min)
 glimpse(raw_15min)
 dfSummary(raw_15min)
+
+#calling columns (x,y) row,column
+raw_15min[2, ]
+raw_15min[ ,2]
+raw_15min[2,3]
+
+mean(raw_15min$speed)
+mean(raw_15min$volume)
+hist(raw_15min$volume)
+
+#learn about filters (use control shift m to add the pipe)
+occ_20plus <- raw_15min |> 
+  filter(occupancy > 20)
+
+occ10_sp80 <- raw_15min |> 
+  filter(occupancy < 10 & speed > 80)
+
+table(occ10_sp80$detector_id)
