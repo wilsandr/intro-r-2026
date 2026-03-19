3cbca7c4c59b17cbf28af37e53bf1c3c4eba642c
Wilson <WilsAndr@trimet.org>
2026-03-19 12:25:55 -0700

    libraries and functions

---
+++ R/Libraries and Functions.R
@@ -0,0 +1,24 @@
+#load libraries
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(plotly)
+
+#load clean dataset
+clean_df <- readRDS("data/clean_data.rds")
+
+speed_occ_fig <- clean_df  |> 
+  filter(detector_id <101100) |> 
+  filter(speed > 40) |> 
+  ggplot(aes(x = speed, y = occupancy, colour = as.factor(detector_id))) +
+  geom_point()
+speed_occ_fig
+
+speed_vol_fig <- clean_df |> 
+  ggplot(aes(x = speed, y = volume, colour = as.factor(detector_id))) +
+  geom_point()
+speed_vol_fig
+
+det_speed_figly <- ggplotly(speed_occ_fig)
+det_speed_figly
+
